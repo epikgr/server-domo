80d28315a3d11dde8649af810009c8ba91f1fd13
epikgr <k.grabiner@epiconcept.fr>
2018-05-13 19:28:12 +0200

    update

--- cache/tmp/header.ba957b7352627c9fcf16970bb31697a8.rtpl.php
+++ cache/tmp/header.ba957b7352627c9fcf16970bb31697a8.rtpl.php
@@ -1,48 +1,25 @@
 <?php if(!class_exists('raintpl')){exit;}?><!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
-    <title>HCC: House Control Center</title>
+    <title>Serveur Domo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <!-- Le styles -->
-    <link href="./templates/hcc/././css/bootstrap.css?v=1" rel="stylesheet">
-    <link href="./templates/hcc/././css/bootstrap-responsive.css?v=1" rel="stylesheet">
-    <script src="./templates/hcc/././js/jquery.min.js?v=1"></script>
-  </head>
+    <link href="./templates/hcc/././css/bootstrap.css?1" rel="stylesheet">
+    <link href="./templates/hcc/././css/bootstrap-responsive.css?1" rel="stylesheet">
+    <script src="./templates/hcc/././js/jquery.min.js?1"></script>
 
-  <body>
+    <style>
+        .container, .panel, .panel-body {
+            padding-left: 0;
+            padding-right: 0;
+        }
+    </style>
+</head>
 
-    <!--<div class="navbar navbar-inverse navbar-fixed-top">-->
-      <!--<div class="navbar-inner">-->
-        <!--<div class="container">-->
-          <!--<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">-->
-            <!--<span class="icon-bar"></span>-->
-            <!--<span class="icon-bar"></span>-->
-            <!--<span class="icon-bar"></span>-->
-          <!--</a>-->
+<body>
 
-          <!--<div class="nav-collapse collapse">-->
-            <!--<ul class="nav">-->
-              <!--<li class="active"><a href="index.php">Accueil</a></li>-->
-              <!--<li><a href="settings.php">Configuration</a></li>-->
-              <!--<li><a href="http://blog.idleman.fr">A propos</a></li>-->
-            <!--</ul>-->
-			<!--<?php if( !isset($myUser) ){ ?>-->
-            <!--<form action="action.php?action=LOGIN" method="POST" class="navbar-form pull-right">-->
-              <!--<input class="span2" name="email" type="text" placeholder="Email">-->
-              <!--<input class="span2"  name="password" type="password" placeholder="Password">-->
-              <!--<button type="submit" class="btn">Connexion</button>-->
-            <!--</form>-->
-			<!--<?php }else{ ?>-->
-			<!--<div class="navbar-form pull-right">-->
-			<!--<a href="action.php?action=DISCONNECT" class="btn">D&eacute;connexion</a>-->
-			<!--</div>-->
-			<!--<?php } ?>-->
-          <!--</div>&lt;!&ndash;/.nav-collapse &ndash;&gt;-->
-        <!--</div>-->
-      <!--</div>-->
-    <!--</div>-->
-
-    <div class="container">
+<div class="container">
